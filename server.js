3795dbda4c1d273d537c0e983305a435a7fb5dfd
HolyAcorn <74989722+HolyAcorn@users.noreply.github.com>
2025-01-20 11:50:00 +0100

    added Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log("listening to " + PORT);
+app.get("/", (req, res) => {
+    res.send("Welcome to my mongoAPI");
 });
+
+app.post("/blogpost", (req, res) => { 
+    res.send("This is a post request");
+});
+
+mongoose.
+connect("").
+then(() => {
+    console.log("connected to Mongo");
+    
+    app.listen(PORT, () => {
+        console.log("listening to " + PORT);
+    });
+    
+    });
