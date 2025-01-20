04cbaa91515ac9e679a561d771cd32cfd96e9a1e
HolyAcorn <74989722+HolyAcorn@users.noreply.github.com>
2025-01-20 15:05:49 +0100

    add api

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 3000;
+const Product = require("./models/productModel");
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to my mongoAPI");
@@ -11,8 +15,51 @@ app.post("/blogpost", (req, res) => {
     res.send("This is a post request");
 });
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get("/api/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product) return res.status(400).json({ message: "Product does not exist" });
+        
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.post("/api/product", async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(404);
+    }
+});
+
 mongoose.
-connect("").
+connect(process.env.MONGO_URI).
 then(() => {
     console.log("connected to Mongo");
     
@@ -20,4 +67,4 @@ then(() => {
         console.log("listening to " + PORT);
     });
     
-    });
+});
